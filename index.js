62adbb7d36a7c5bda09abc094b853cb80def3daa
Ronald C Edwards <rce246@stern.nyu.edu>
2020-01-17 20:31:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var recipes = {"Ingredient1":"Eggs", "Ingredient2":"Butter", "Ingredient3":"Flour"}
 function updateObjectWithKeyAndValue(object, key, value){return Object.assign({}, object, {[key]:value})}
-function destructivelyUpdateObjectWithKeyandValue(object, key, value){object[key] = value; return object}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){object[key] = value; return object}
